3c0a183d0a242368b4b40b7b391564a7cc8a394b
Carson <carsonbro640@gmail.com>
2026-02-14 18:43:47 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import { startEmailWatchers } from "./emailWatcher.js";
+import config from "./config.json" assert { type: "json" };
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", async () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+  await startEmailWatchers(client, config);
+});
+
+client.login(config.discord.token);
