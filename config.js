552e0c7dd05ce034dddda81cc12761d53211b28f
Carson <carsonbro640@gmail.com>
2026-02-14 21:07:09 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,31 +1,17 @@
-
-import "dotenv/config";
-
 export default {
   discord: {
-    token: process.env.BOT_TOKEN,
-    channelId: "1472388183529492583"
+    token: process.env.BOT_TOKEN
   },
-
-  gmail: {
-    host: "imap.gmail.com",
-    port: 993,
-    secure: true,
-    user: "carsonidler6901@gmail.com",
-    pass: process.env.GMAIL_PASS
-  },
-
-  zoho: {
-    host: "imap.zoho.com",
-    port: 993,
-    secure: true,
-    user: "cidler6901@zohomail.com",
-    pass: process.env.ZOHO_PASS
-  },
-
-  filters: {
-    ignoreSubjects: [],
-    ignoreSenders: [],
-    keywords: []
-  }
+  emails: [
+    {
+      service: "gmail",
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+    },
+    {
+      service: "outlook",
+      user: process.env.OUTLOOK_USER,
+      pass: process.env.OUTLOOK_PASS
+    }
+  ]
 };
